a13d2cbac80f7b82c9859f251e2432d90da3b122
Jeremy Xu <jxu14@Jeremys-MacBook-Pro.local>
2023-04-13 15:46:18 -0500

    add adoption details to results page

--- cgi-bin/matcher_script.py
+++ cgi-bin/matcher_script.py
@@ -90,6 +90,16 @@ def format_animals(animals):
         animal_type = animal['type']
         primary_breed = animal['breeds']['primary']
         age = animal['age']
+        link = animal['url']
+        gender = animal['gender']
+        contact = animal['contact']
+        email = contact['email']
+        phone = contact['phone']
+        address = contact['address']
+        conv = lambda i: i or ''
+        street_num = conv(address['address1'])
+        formatted_address = street_num + " " + str(address['city']) + ", " + str(address['state']) + " " + str(address['postcode'])
+
 
         # format the information in a box
         box = f'''
@@ -100,6 +110,16 @@ def format_animals(animals):
                 <p>Type: {animal_type}</p>
                 <p>Primary Breed: {primary_breed}</p>
                 <p>Age: {age}</p>
+                <p>Gender: {gender} </p>
+                <h3>Contact info</h3>
+                <span>&ensp;Email: {email} </span>
+                <br>
+                <span>&ensp;Phone Number: {phone} </span>
+                <br>
+                <span>&ensp;Address: {formatted_address} </span>
+                <br>
+                <br>
+                <a href="{link}">Click here to learn more about {name}</a>
             </div>
         '''
 
@@ -113,6 +133,7 @@ print("Content-Type: text/html")    # Set the content type of the response
 print()    # Print an empty line to indicate the end of the headers
 print("<html>")
 print("<head>")
+print("<link rel=\"stylesheet\" type=\"text/css\" href=\"../styles.css\" media=\"screen\"/>")
 print("<title>PetMatcher Results</title>")
 print("</head>")
 print("<body>")
